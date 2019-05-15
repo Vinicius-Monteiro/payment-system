b6ab3818b1e92e3ba0a30bd879f3d53036cd47a6
Vinicius-Monteiro <viniciusmp-7@hotmail.com>
2019-05-15 19:21:02 -0300

    Matrix is now dynamic

--- Main.java
+++ Main.java
@@ -1,14 +1,35 @@
 import java.util.Scanner;
 
 public class Main {
-	public static boolean add(String name, String address, String contract, String attribute, String id, int index, String [][]employees) {
-		if(employees[index][0] != null) return false;
+	public static void add(String name, String address, String contract, String attribute, String id, int index, String [][]employees) {
 		employees[index][0] = name;
 		employees[index][1] = address;
 		employees[index][2] = contract;
 		employees[index][3] = attribute;
 		employees[index][4] = id;
-		return true;
+		return;
+	}
+
+	public static int findIndex(int size, String name, String [][]employees) {
+		for(int i = 0; i < size; i++) {
+			if(employees[i][0] != null && employees[i][0] == name) return i;
+		}
+		return -1;//employee not found
+	}	
+
+	public static void remove(int size, String name, String [][]employees) {
+		int index = findIndex(size, name, employees);
+		if(index == -1)
+			System.out.println("Employee not found");
+		else {
+			while(employees[index + 1][0] != null) {
+				for(int i = 0; i < 5; i++) {
+					employees[index][i] = employees[index + 1][i];
+					employees[index + 1][i] = null;
+				}
+				index++;
+			}
+		}
 	}
 
 	public static void getAt(int index, String [][]employees) {
@@ -20,21 +41,23 @@ public class Main {
 		return;
 	}
 
-	public static String[][] growMatrix(int size, String [][]employees) {//returns a matrix 20% larger than the previous, copying its content
-		int newSize = (int)(size * 1.2), i = 0;
-		String [][]newString = new String[newSize][5];
-		while(employees[i][0] != null && i < size) {
-			newString[i][0] = employees[i][0];
-			newString[i][1] = employees[i][1];
-			newString[i][2] = employees[i][2];
-			newString[i][3] = employees[i][3];
-			newString[i][4] = employees[i][4];
+	public static void printAll(int size, String [][]employees) {
+		for(int i = 0; i < size; i++) {
+			System.out.println("||||||||||||||");
+			getAt(i, employees);
 		}
-		return newString;
+		return;
 	}
+
+	public static String[][] growMatrix(String [][]employees) {//returns a matrix 50% larger than the previous, with the same content
+		int newSize = (int)(employees.length * 1.5) + 1;
+		String [][]newMatrix = new String[newSize][5];
+		System.arraycopy(employees, 0, newMatrix, 0, employees.length);
+		return newMatrix;
+	}
+
 	public static void main(String[] args) {
-		String usage = "Forneça um comando (1-10):\n"
-						+"\t1- Adiçao de um empregado\n"
+		String usage = "\t1- Adiçao de um empregado\n"
 						+"\t2- Remoçao de um empregado\n"
 						+"\t3- Lançar um cartao de ponto\n"
 						+"\t4- Lançar um resultado de venda\n"
@@ -45,32 +68,53 @@ public class Main {
 						+"\t9- Agenda de pagamento\n"
 						+"\t10- Criação de novas agendas de pagamento\n";
 
-		int size = 50, employeeCount = 0, globalId = 0;
-		String [][]employees = new String[size][5];
+		System.out.print("Açoes do programa:\n"
+						+"\tl- Listar comandos do sistema\n"
+						+"\t[1,10]- Comandos\n"
+						+"\tq- Terminar programa\n");
 		
-		System.out.print(usage + ":");
-		Scanner in = new Scanner(System.in);
-		String command = in.nextLine();
+		int size = 20, employeeCount = 0, globalId = 1;
+		String [][]employees = new String[size][5];
+		while(true) {
+			System.out.print(":");
+			Scanner in = new Scanner(System.in);
+			String command = in.nextLine();
 
-		switch(command.charAt(0)) {
-			case '1':
-				System.out.println("Forneça as seguintes informações separadas em linhas separadas:\n"
-								+"\tNome, endereço, tipo de contrato (hourly, salaried, commissioned)\n"
-								+"\te os atributos associados (salario horario, salario mensal, comissao)");
-				String name = in.nextLine();
-				String address = in.nextLine();
-				String contract = in.nextLine();
-				String attribute = in.nextLine();
-				
-				if(employeeCount == size) {
-					employees = growMatrix(size, employees);
-					size = (int)(size * 1.2);
-				}
-				
-				add(name, address, contract, attribute, Integer.toString(globalId), employeeCount, employees);
-				employeeCount++;
-				globalId++;
-				break;
+			switch(command.charAt(0)) {
+				case 'q':
+					return;
+				case 'l':
+					System.out.print(usage);
+					break;
+				case '1':
+					System.out.println("Forneça as seguintes informações:");
+					System.out.print("\tNome: ");
+					String name = in.nextLine();
+					System.out.print("\tEndereço: ");
+					String address = in.nextLine();
+					System.out.print("\tContrato: ");
+					String contract = in.nextLine();
+					System.out.print("\tAtributo associado: ");
+					String attribute = in.nextLine();
+					
+					if(employeeCount == size) {
+						employees = growMatrix(employees);
+						size = employees.length;
+					}
+					
+					add(name, address, contract, attribute, Integer.toString(globalId), employeeCount, employees);
+					employeeCount++;
+					globalId++;
+					break;
+				case '2':
+					System.out.print("Forneça o nome do funcionario que deseja remover\n"
+									+"\t:");
+					String name2 = in.nextLine();
+					remove(size, name2, employees);
+					employeeCount--;
+
+			}
+			//printAll(size, employees);
 		}
 	}
 }
